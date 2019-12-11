18b039b55cd2c1a228d0cd361d6bd973220cb431
Techno-Disaster <nitinnirve@gmail.com>
2019-12-11 15:39:55 +0530

    maze ready

---
+++ lib/main.dart
@@ -0,0 +1,114 @@
+import 'dart:async';
+import 'package:flutter/material.dart';
+import 'maze.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) => MaterialApp(
+        title: 'Mazegen',
+        theme: ThemeData(primarySwatch: Colors.blue),
+        home: HomePage(),
+      );
+}
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  var _maze;
+  Iterator<Cell> _counter;
+
+  @override
+  void initState() {
+    super.initState();
+    go();
+  }
+
+  void go() {
+    _maze = Maze(
+        17, 17); // increase this if u want to increase complexity of the maze.
+    _counter = _maze.generate().iterator;
+    Timer.periodic(Duration(milliseconds: 1), onTick);
+  }
+
+  void onTick(Timer timer) {
+    if (_counter.moveNext()) {
+      setState(() {});
+    } else {
+      timer.cancel();
+      _counter = null;
+      setState(() {});
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) => Scaffold(
+        backgroundColor: Colors.grey,
+        appBar: AppBar(
+          title: Text(
+            'MazeMate',
+            style: TextStyle(color: Colors.black),
+          ),
+          backgroundColor: Colors.grey,
+          centerTitle: true,
+        ),
+        floatingActionButton: FloatingActionButton(
+          backgroundColor: Colors.amber,
+          onPressed: _counter == null ? go : null,
+          child: Icon(Icons.refresh, color: Colors.black),
+        ),
+        body: Center(
+          child: AspectRatio(
+            aspectRatio: 1,
+            child: Column(
+              children: [
+                for (var row = 0; row != _maze.rows; ++row)
+                  Expanded(
+                    child: Row(
+                      children: [
+                        for (var col = 0; col != _maze.cols; ++col)
+                          Expanded(
+                            child: CellView(
+                              _maze.getCell(row,
+                                  col), // Cellview sets border so that maze looks complete.
+                            ),
+                          ),
+                      ],
+                    ),
+                  ),
+              ],
+            ),
+          ),
+        ),
+      );
+}
+
+class CellView extends StatelessWidget {
+  final Cell cell;
+  CellView(this.cell);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        border: Border(
+          top: _getBorderSide(Wall.top),
+          left: _getBorderSide(Wall.left),
+          right: _getBorderSide(Wall.right),
+          bottom: _getBorderSide(Wall.bottom),
+        ),
+        color: cell.visited ? Colors.green : Colors.black,
+      ),
+    );
+  }
+
+  BorderSide _getBorderSide(Wall wall) => BorderSide(
+      width: 5.0,
+      style: cell.wallon[wall.index] ? BorderStyle.solid : BorderStyle.none);
+}
---
+++ lib/maze.dart
@@ -0,0 +1,124 @@
+import 'dart:math';
+
+enum Wall {
+  top,
+  right,
+  bottom,
+  left,
+}
+
+class Cell {
+  final int row;
+  final int col;
+  var visited = false;
+  var wallon = List<bool>(Wall.values.length);
+
+  Cell(this.row, this.col) {
+    for (var i = 0; i != wallon.length; ++i) wallon[i] = true;
+  }
+}
+
+class Move {
+  final Cell cellFrom;
+  final Cell cellTo;
+
+  Move(this.cellFrom, this.cellTo);
+}
+
+class Maze {
+  final int rows;
+  final int cols;
+  final List<Cell> _cells;
+  final _skippedMoves = List<Move>();
+  final _rand = Random();
+
+  int _offset(int row, int col) => row * rows + col;
+
+  Maze(this.rows, this.cols) : _cells = List<Cell>(rows * cols) {
+    for (var col = 0; col < cols; col++) {
+      for (var row = 0; row < rows; row++) {
+        _cells[_offset(row, col)] = Cell(row, col);
+      }
+    }
+  }
+
+  Cell getCell(int row, int col) =>
+      row < 0 || row >= rows || col < 0 || col >= cols
+          ? null
+          : _cells[_offset(row, col)];
+
+  Cell _adjacentCell(Cell cellFrom, Wall wall) {
+    switch (wall) {
+      case Wall.top:
+        return getCell(cellFrom.row - 1, cellFrom.col);
+      case Wall.left:
+        return getCell(cellFrom.row, cellFrom.col - 1);
+      case Wall.bottom:
+        return getCell(cellFrom.row + 1, cellFrom.col);
+      case Wall.right:
+        return getCell(cellFrom.row, cellFrom.col + 1);
+      default:
+        assert(false);
+        return null;
+    }
+  }
+
+  // Maze Generation
+  Iterable<Cell> generate() sync* {
+    // Choose cell at random starting point
+    var cell = getCell(_rand.nextInt(rows), _rand.nextInt(cols));
+
+    while (true) {
+      cell.visited = true;
+
+      yield cell;
+      var possibleCellsTo = Wall.values
+          .map((w) => _adjacentCell(cell, w))
+          .where((c) => c != null && !c.visited)
+          .toList();
+      Move nextMove;
+
+      // If there is a possible cell to move to from the current cell
+      if (possibleCellsTo.isNotEmpty) {
+        //  Choose random cell to move to
+        nextMove =
+            Move(cell, possibleCellsTo[_rand.nextInt(possibleCellsTo.length)]);
+
+        // Remember possible moves we skipped for future consideration
+        _skippedMoves.addAll(
+          possibleCellsTo
+              .where(
+                (c) => !identical(c, nextMove.cellTo),
+              )
+              .map(
+                (c) => Move(cell, c),
+              ),
+        );
+      }
+      // If there are no possible moves from the current cell
+      // check list of skipped moves for a "to" cell not already visited.
+      else {
+        nextMove = _skippedMoves.firstWhere((m) => !m.cellTo.visited,
+            orElse: () => null);
+      }
+
+      // If there is a move
+      if (nextMove != null) {
+        // Figure out the walls to knock down
+        var wallFrom = Wall.values.firstWhere((w) =>
+            identical(_adjacentCell(nextMove.cellFrom, w), nextMove.cellTo));
+        var wallTo = Wall.values[(wallFrom.index + 2) % Wall.values.length];
+
+        // Knock down wall between the cell we're moving from to the cell we're moving to
+        nextMove.cellFrom.wallon[wallFrom.index] = false;
+        nextMove.cellTo.wallon[wallTo.index] = false;
+
+        // Do the move and continue
+        cell = nextMove.cellTo;
+      } else {
+        // we're done
+        break;
+      }
+    }
+  }
+}
